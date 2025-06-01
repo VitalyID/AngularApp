90229ebe86b36a7fd6355dd9e0fb82e9b40bb549
Vitaly <s25698@ya.ru>
2025-06-02 01:03:44 +0500

    added routing in home-component

--- src/app/ app.routes.ts
+++ src/app/ app.routes.ts
@@ -1,17 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmptyRouteComponent } from './components/empty-route/empty-route.component';
-import { MainComponent } from './components/main/main.component';
+import { HomeComponent } from './components/layouts/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: MainComponent,
-    // component: MainModule,
-    // component: MainRoutingModule,
-
-    // loadChildren: () =>
-    //   import('./components/main/main.module').then((m) => m.MainModule),
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./components/main/main.module').then((m) => m.MainModule),
+      },
+      {
+        path: 'my-qr',
+        loadChildren: () =>
+          import('./components/myQR/my-qr.module').then(
+            (m) => m.MyQRComponentModule
+          ),
+      },
+      {
+        path: 'create-qrcode/:id',
+        loadChildren: () =>
+          import('./components/QR-CodeCreator/qr-code-creator.module').then(
+            (m) => m.CreateQrcodeModule
+          ),
+      },
+      {
+        path: 'create-qrcode',
+        loadChildren: () =>
+          import('./components/QR-CodeCreator/qr-code-creator.module').then(
+            (m) => m.CreateQrcodeModule
+          ),
+      },
+      {
+        path: 'agents',
+        loadChildren: () =>
+          import('./components/agents/agents.module').then(
+            (m) => m.AgentsModule
+          ),
+        // data: { asideID: 3 },
+      },
+    ],
   },
 
   {
@@ -19,35 +50,7 @@ export const routes: Routes = [
     redirectTo: '',
     pathMatch: 'full',
   },
-  {
-    path: 'my-qr',
-    loadChildren: () =>
-      import('./components/myQR/my-qr.module').then(
-        (m) => m.MyQRComponentModule
-      ),
-  },
-  {
-    path: 'create-qrcode/:id',
-    loadChildren: () =>
-      import('./components/QR-CodeCreator/qr-code-creator.module').then(
-        (m) => m.CreateQrcodeModule
-      ),
-    // data: { asideID: 1 },
-  },
-  {
-    path: 'create-qrcode',
-    loadChildren: () =>
-      import('./components/QR-CodeCreator/qr-code-creator.module').then(
-        (m) => m.CreateQrcodeModule
-      ),
-    // data: { asideID: 1 },
-  },
-  {
-    path: 'agents',
-    loadChildren: () =>
-      import('./components/agents/agents.module').then((m) => m.AgentsModule),
-    // data: { asideID: 3 },
-  },
+
   {
     path: '**',
     component: EmptyRouteComponent,
