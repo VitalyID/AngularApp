2b8c0e45a7931339d92cdd41c652b07343efd64d
Vitaly <s25698@ya.ru>
2025-09-02 07:01:24 +0500

    changed pipe bankCardNumberSpaces edit args and types

--- src/app/shared/components/bank-card/pipe/card-number.ts
+++ src/app/shared/components/bank-card/pipe/card-number.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class BankCardNumberSpaces implements PipeTransform {
-  transform(value: string | null, ...args: any[]): string | undefined {
-    if (!value) return ' ';
+  transform(value: string): string {
+    if (!value?.length) return '';
     const strValue = value.toString();
     return strValue.replace(/(\d{4})(?=\d)/g, '$1 ');
   }
